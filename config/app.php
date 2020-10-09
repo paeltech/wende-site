4b73fc405865697ee10ae70931df58d2cba6858a
Paul Mandele <Paul@Pauls-Macbook-Pro.local>
2020-10-09 08:00:10 +0300

    Added voyager service provider

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        TCG\Voyager\VoyagerServiceProvider,
+
         /*
          * Package Service Providers...
          */
